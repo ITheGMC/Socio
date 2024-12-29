8e669b545c7705a01ed3d36733fd23e292f41e1f
ITheGMC <133953793+ITheGMC@users.noreply.github.com>
2024-12-30 00:06:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,61 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = socketIo(server);
 
-app.use(express.static('static'));
+let rooms = {};  // A simple object to store room details
 
-let rooms = {};
+// Serve static files (Frontend)
+app.use(express.static('public'));
 
+// When a user connects
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('a user connected');
 
-    socket.on('join-room', ({ name, roomCode }) => {
-        socket.join(roomCode);
-        rooms[roomCode] = rooms[roomCode] || [];
-        rooms[roomCode].push({ id: socket.id, name });
-        io.to(roomCode).emit('update-status', `${name} has joined the room.`);
+    // Listen for 'joinRoom' event from frontend
+    socket.on('joinRoom', ({ code, name }) => {
+        if (!rooms[code]) {
+            // If the room doesn't exist, create a new room
+            rooms[code] = { users: [] };
+        }
 
-        socket.on('send-message', (msg) => {
-            socket.to(roomCode).emit('receive-message', `${name}: ${msg}`);
-        });
+        // Add user to the room
+        rooms[code].users.push({ id: socket.id, name });
 
-        socket.on('typing', (isTyping) => {
-            socket.to(roomCode).emit('typing', isTyping);
-        });
+        // Join the room
+        socket.join(code);
 
+        // Send welcome message
+        socket.emit('message', `Welcome ${name} to the chat!`);
+
+        // Notify the other user in the room
+        socket.to(code).emit('message', `${name} has joined the chat`);
+
+        console.log(`${name} joined room ${code}`);
+
+        // Handle disconnection
         socket.on('disconnect', () => {
-            rooms[roomCode] = rooms[roomCode].filter((user) => user.id !== socket.id);
-            io.to(roomCode).emit('update-status', `${name} has disconnected.`);
+            rooms[code].users = rooms[code].users.filter(user => user.id !== socket.id);
+            if (rooms[code].users.length === 0) {
+                delete rooms[code];  // Delete room if no users left
+            }
+            console.log(`${name} left room ${code}`);
         });
+    });
+
+    // Listen for 'sendMessage' event to broadcast messages in the room
+    socket.on('sendMessage', (message) => {
+        const roomCode = Object.keys(rooms).find(code => rooms[code].users.some(user => user.id === socket.id));
+        if (roomCode) {
+            io.to(roomCode).emit('message', message);
+        }
     });
 });
 
+// Start the server
 server.listen(3000, () => {
-    console.log('Server Successfully running on http://localhost:3000');
+    console.log('Server is running on port 3000');
 });
